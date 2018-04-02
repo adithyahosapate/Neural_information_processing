03f7f0f999ab377f52fc028ed2a2c7bb8d19f012
adithyahosapate <adihosapate@gmail.com>
2018-04-02 23:09:13 +0530

    added data

--- markov.py
+++ markov.py
@@ -1,34 +1,31 @@
 import numpy as np
 
+np.random.seed(10)
+initial_seed='1010101010'
+iterations=10000000
+k=10
 
-y_1x_1=0.5
-y_1x_0=0.5
-y_0x_0=0.5
-y_0x_1=0.5
+transition_probabilities=np.random.uniform(0,1,int(2**k))
+
+transition_matrix=np.vstack([transition_probabilities,1-transition_probabilities])
+
+
+print(transition_matrix)
+
+with open('data.txt','w') as f:
+	f.write(initial_seed)
 
 
 
-initial= 0
-print(initial,end=" ")
-current_state=initial
-for i in range(100):
-	if current_state==1:
-		if (np.random.uniform()<y_0x_1):
-			current_state=0
-		else:
-			current_state=1
+f=open('data.txt','w')
+
+print(int(str(initial_seed),2))
+current_state=initial_seed
+for i in range(iterations):
+	index=int(str(current_state),2)
+	if np.random.uniform()<transition_matrix[0][index]:
+		current_state=current_state[1:]+'0'
+		f.write('0')
 	else:
-		if (np.random.uniform()<y_0x_0):
-			current_state=0
-		else:
-			current_state=1
-	print(current_state,end=" ")			
-
-
-
-
-				
-
-
-
-
+		current_state=current_state[1:]+'1'
+		f.write('1')
