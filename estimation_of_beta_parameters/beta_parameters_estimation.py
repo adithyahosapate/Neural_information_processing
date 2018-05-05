b3c2df35456592f81c389d55ae16a9e1aebbb7bc
deep529 <ddiwani3008@gmail.com>
2018-05-05 13:00:16 +0530

    Added Annotations in graph

--- estimation_of_beta_parameters/beta_parameters_estimation.py
+++ estimation_of_beta_parameters/beta_parameters_estimation.py
@@ -25,17 +25,30 @@ n = bitstring.shape[0]
 
 x = np.linspace(beta.ppf(0.01, ALPHA+n1, BETA+n-n1),
  				beta.ppf(0.99, ALPHA+n1, BETA+n-n1), 100)
-
+# x = np.linspace(0, 1, 500)
 # x = np.linspace(beta.ppf(0.01, ALPHA, BETA),
 #  				beta.ppf(0.99, ALPHA, BETA), 100)
 
 
 a = max(zip(beta.pdf(x, ALPHA+n1, BETA+n-n1),x))[1]
 print(a)
-print(n1/n)
-# plt.plot(x, beta.pdf(x, ALPHA+n1, BETA+n-n1))
-# plt.plot(x, beta.pdf(x, ALPHA, BETA), 'r-')
-# plt.show()
+print(n1 / n)
 
-plt.plot(range(700),np.random.binomial(size=700,n=1,p=a))
+# to plot pdf uncomment following & comment all after plt.show()
+""" post_plot, = plt.plot(x, beta.pdf(x, ALPHA+n1, BETA+n-n1), label="Posterior PDF")
+prior_plot, = plt.plot(x, beta.pdf(x, ALPHA, BETA), 'r-', label="Prior PDF")
+plt.xlabel("Bernoulli Variable ${\Theta}$")
+plt.ylabel("Probability Density")
+plt.title("PDF of Posterior & Prior")
+plt.legend(bbox_to_anchor=(0.66, 1), loc=2, borderaxespad=0.)
+plt.savefig("pdf_zoomed.png")
+plt.show() """
+
+
+""" To plot Spikes """
+plt.plot(range(700), np.random.binomial(size=700, n=1, p=a))
+plt.xlabel("Time")
+plt.ylabel("Spike")
+plt.title("Spikes using Bernoulli model with Beta as Prior")
+plt.savefig("spikes_using_estimated_from_beta.png")
 plt.show()
